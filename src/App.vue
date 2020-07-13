f589853d6b4114b2ba21eb40fb7dd3e30eba1954
LUCAS VASCONCELOS <lucas.av7@yahoo.com>
2020-07-13 11:52:11 -0400

    Adicionando esqueleto dos componente iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
-    
+    <h1 class="titulo">Lista de tarefas</h1>
+    <NewTask />
+    <TaskGrid :tasks="tasks" />
   </div>
 </template>
 
 <script>
+import NewTask from './components/NewTask.vue'
+import TaskGrid from './components/TaskGrid.vue'
 
 export default {
   name: 'App',
-  components: {}
+  components: { NewTask, TaskGrid },
+  data() {
+    return {
+      tasks: [
+        { name: 'Jogar o lixo', pending: true },
+        { name: 'Jogar bola', pending: false },
+      ]
+    }
+  }
 }
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
 
+  body {
+    background: #DBE6F6;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #C5796D, #DBE6F6);
+    background: linear-gradient(to right, #C5796D, #DBE6F6);
+    font-family: 'Roboto', sans-serif;
+    font-size: 62.5%;
+  }
+
+  #app {
+    max-width: 960px;
+    min-height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .titulo {
+    font-size: 3.0rem;
+  }
 </style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tasks">
+    <Task v-for="task in tasks" :key="task.name" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true }
+  }
+}
+</script>
+
+<style>
+
+</style>
